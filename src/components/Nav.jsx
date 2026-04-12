0c0ad4e7569f503a280998a05a55e81d286edfd1
obdaguy-create <obdaguy@gmail.com>
2026-04-12 18:44:23 +0400

    Add files via upload

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,1 +1,17 @@
+import { NavLink } from 'react-router-dom'
 
+export default function Nav() {
+  return (
+    <nav className="nav">
+      <a href="/" className="nav-brand">AGRO<span>FLY</span></a>
+      <div className="nav-links">
+        <NavLink to="/"         className={({isActive})=>"nav-link"+(isActive?" active":"")}>
+          Farmer Portal
+        </NavLink>
+        <NavLink to="/operator" className={({isActive})=>"nav-link"+(isActive?" active":"")}>
+          Operator Dispatch
+        </NavLink>
+      </div>
+    </nav>
+  )
+}
